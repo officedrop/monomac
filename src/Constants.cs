c86ccb1bcb35489c3173e7792b687923a7886d46
Victor Hugo <hey.zooi@gmail.com>
2012-05-18 13:11:23 -0300

    adding LaunchServices Library

--- src/Constants.cs
+++ src/Constants.cs
@@ -57,6 +57,5 @@ namespace MonoMac {
 		public const string QuickLookLibrary = "/System/Library/Frameworks/QuickLook.framework/QuickLook";
 		public const string AVFoundationLibrary = "/System/Library/Frameworks/AVFoundation.framework/AVFoundation";
 		public const string LaunchServicesLibrary = "/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/LaunchServices";
-
     }
 }
